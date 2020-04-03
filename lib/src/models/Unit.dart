abcdfc891aeab9867219986dac72ea23b08bf791
jaysavsani07 <jay@nividata.com>
2020-04-03 20:38:55 +0530

    converter done with output showing correct value, mm need to add input via rx

--- lib/src/models/Unit.dart
+++ lib/src/models/Unit.dart
@@ -3,14 +3,21 @@ import 'package:meta/meta.dart';
 class Unit {
   final String name;
   final double conversion;
+  final String shortName;
 
-  const Unit({@required this.name, @required this.conversion})
+  const Unit(
+      {@required this.name,
+      @required this.conversion,
+      @required this.shortName})
       : assert(name != null),
-        assert(conversion != null);
+        assert(conversion != null),
+        assert(shortName != null);
 
   Unit.fromJson(Map jsonMap)
       : assert(jsonMap['name'] != null),
         assert(jsonMap['conversion'] != null),
+        assert(jsonMap['short_name'] != null),
         name = jsonMap['name'],
-        conversion = jsonMap['conversion'].toDouble();
+        conversion = jsonMap['conversion'].toDouble(),
+        shortName = jsonMap['short_name'];
 }
--- lib/src/ui/ConverterCalc.dart
+++ lib/src/ui/ConverterCalc.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:convert/convert.dart';
 import 'package:calculator/src/models/Category.dart';
@@ -12,11 +13,14 @@ class ConverterCalc extends StatefulWidget {
 
 class _ConverterCalcState extends State<ConverterCalc> {
   Color _textColor;
-
   final _categories = <Category>[];
-  Category _defaultCategory;
   Category _currentCategory;
   final _selectedChip = <bool>[];
+  List<DropdownMenuItem> _unitMenuItems;
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue = 1;
+  String _convertedValue = '';
 
   @override
   Future<void> didChangeDependencies() async {
@@ -37,20 +41,20 @@ class _ConverterCalcState extends State<ConverterCalc> {
     }
     var categoryIndex = 0;
     unitsData.keys.forEach((key) {
-      print(key);
       final List<Unit> units = unitsData[key]
           .map<Unit>((dynamic data) => Unit.fromJson(data))
           .toList();
       var category = Category(name: key, units: units);
       setState(() {
         if (categoryIndex == 0) {
-          _defaultCategory = category;
+          _currentCategory = category;
         }
         _categories.add(category);
         _selectedChip.add(false);
       });
       categoryIndex++;
     });
+    _createDropdownMenuItems(_currentCategory);
     _selectedChip[0] = true;
   }
 
@@ -63,7 +67,96 @@ class _ConverterCalcState extends State<ConverterCalc> {
 
   void _onCategoryTap(Category category) {
     print("ouch i was tapped ${category.name}");
-    _currentCategory = category;
+    setState(() {
+      _currentCategory = category;
+    });
+    _createDropdownMenuItems(_currentCategory);
+  }
+
+  void _createDropdownMenuItems(Category selectedCategory) {
+    var newItems = <DropdownMenuItem>[];
+    for (var unit in selectedCategory.units) {
+      newItems.add(DropdownMenuItem(
+        value: unit.shortName,
+        child: Container(
+          child: Text(
+            unit.shortName,
+            softWrap: true,
+          ),
+        ),
+      ));
+    }
+    setState(() {
+      _unitMenuItems = newItems;
+      _fromValue = selectedCategory.units[0];
+      _toValue = selectedCategory.units[1];
+    });
+  }
+
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      _toValue = _getUnit(unitName);
+      print(_toValue.name);
+    });
+
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  void _updateFromConversion(dynamic unitShortName) {
+    setState(() {
+      _fromValue = _getUnit(unitShortName);
+    });
+
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  Future<void> _updateConversion() async {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+    print(_convertedValue);
+  }
+
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+
+    return outputNum;
+  }
+
+  Unit _getUnit(String unitShortName) {
+    return _currentCategory.units.firstWhere(
+      (Unit unit) {
+        return unit.shortName == unitShortName;
+      },
+      orElse: null,
+    );
+  }
+
+  Widget _createDropDownButton(
+      String currentVal, ValueChanged<dynamic> onChanged) {
+    return DropdownButton(
+        onChanged: onChanged,
+        value: currentVal,
+        items: _unitMenuItems,
+        style: Theme.of(context).textTheme.title);
   }
 
   @override
@@ -81,8 +174,34 @@ class _ConverterCalcState extends State<ConverterCalc> {
             child: Container(
               margin: EdgeInsets.fromLTRB(20, 15, 20, 10),
               decoration: BoxDecoration(
-                  color: Color(0xFF480048),
+                  color: Color(0xFF484848),
                   borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 6,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(24, 10, 0, 0),
+                          child: Text(
+                            _fromValue.name,
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: _createDropDownButton(
+                                _fromValue.shortName, _updateFromConversion)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -94,7 +213,38 @@ class _ConverterCalcState extends State<ConverterCalc> {
                   color: Color(0xFF484848),
                   borderRadius: BorderRadius.all(Radius.circular(20.0))),
               child: Column(
-                children: <Widget>[Text("hello")],
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 6,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(24, 10, 0, 0),
+                          child: Text(
+                            _toValue.name,
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: _createDropDownButton(
+                                _toValue.shortName, _updateToConversion)),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    alignment: Alignment.topLeft,
+                    child: Text(_convertedValue,
+                    style: TextStyle(
+                      fontSize: 40
+                    )),
+                  )
+                ],
               ),
             ),
           ),
